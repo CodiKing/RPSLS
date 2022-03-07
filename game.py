17bb8ea29fc7ecc7376f096b5c5926a1771d2a18
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-03-07 16:26:09 -0600

    Added some basic logic theory into the human input

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
+from human import Human
 
 
 class Game:
     def __init__(self):
         pass
 
+    def run_game(self):
+        self.display_greeting()
+        self.display_rules()
+        self.user_gamemode_choice()
+
     def display_greeting():
         print("Hello! Lets play a game!\n")
         print("This game is like Rock, Paper, Scissors... but more fancy!\n")
         print("Rules are, there are no rules. jk\n")
-
 
     def display_rules():
         print("""Explanation of game rules:
@@ -23,11 +28,20 @@ class Game:
              Paper disproves Spock 
              Spock vaporizes Rock""")
 
-    
-    
     def user_gamemode_choice():
         print("How would you like to play this game? With a friend or by yourself against AI?\n")
         user_input = input("Press 0 for multiplayer or 1 for Single player: ")
 
         if user_input == 0:
             pass
+
+    def human_turn(self):
+        self.human_choice()
+
+        pass
+
+    def ai_turn(self):
+        self.ai_choice
+        pass
+
+    def comparing_gestures():
---
+++ human.py
@@ -0,0 +1,16 @@
+from person import Person
+
+
+class Human:
+    def __init__(Person):
+        counter = 0
+
+    def human_choice():
+        print("What would you like to throw??\n")
+        print(f"""Press: 0 for Rock
+                  Press: 1 for Scissors
+                  Press: 2 for Paper
+                  Press: 3 for Lizard
+                  Press: 4 for Spock       """)
+
+        input("Please select a number: ")
